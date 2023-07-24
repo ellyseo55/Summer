a676e4c2f85b9d0129cbf30d0bec979d7b6006ee
ellyseo55 <ellyseo2003@gmail.com>
2023-07-24 11:21:07 +0900

    7_24
    오늘 한 일

---
+++ src/app/component/Login.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Login() {
+    return (
+        <div>
+            <h1>
+                <div>ID</div>
+                <div>PASSWORD</div>
+                <div>SIGN IN</div>
+            </h1>
+        </div>
+    );
+}
